6c13cdf9c603f170f3a12ab8f490ed17a7201972
Rob2729 <Rob.niv@hotmail.com>
2022-02-20 08:52:36 +0000

    Initial Commit

---
+++ YoutubeUI/YoutubeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeUIApp.swift
+//  YoutubeUI
+//
+//  Created by Robert on 20/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
